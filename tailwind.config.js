5da0767f3ce9c4dcd1b8a4e148a496474f1b52ec
Guillaume <guizanko@hotmail.fr>
2024-01-18 15:58:01 +0100

    darklightmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Activer le mode sombre basé sur les classes
   content: ["./*.html", "./*.css"],
   theme: {
     extend: {
-      boxShadow: {
-        glass: '0 4px 30px rgba(0, 0, 0, 0.1)',
-      },
-      backdropFilter: {
-        glass: 'blur(0px)',
-      },
-      backdropBlur: {
-        glass: 'blur(0px)',
-      },
-      backgroundColor: {
-        'lglass': 'rgba(255, 255, 255, 0.04)',
-      },
-      backdropFilter: {
-        'lglass': 'blur(10px)',
-      },
-      borderOpacity: {
-        'lglass': '0.05',
-      },
     },
   },
   plugins: [],
